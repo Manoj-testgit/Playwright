fc9f6ee0d445db0e5eaef74122ea1fdd32597ca1
Manoj Kumar C <manojkumarc2994@gmail.com>
2026-07-27 21:44:24 +0530

    monitering_api

--- tests/Playwright/UIbasics16_monitorapi.spec.js
+++ tests/Playwright/UIbasics16_monitorapi.spec.js
@@ -1,16 +1,25 @@
-const{test,expect} = require("@playwright/test")
+const{test,expect} = require("@playwright/test");
+const { count } = require("node:console");
 const { request } = require("node:http")
 
 
-test("Health monitor API" , async({request}) =>
+test.only("Health monitor API" , async({request}) =>
 {
 
     test.setTimeout(0)//setting the timeout to 0 - no timeout 
 
+    let attempts = 0;
+    const maxAttempts = 10;
 
     while(true)
 
-    {
+    {//add this if condition to limit the number of count to 10
+        attempts = attempts + 1;
+        if (attempts > maxAttempts) {
+            console.log("Reached maximum attempts, count is " +maxAttempts);
+            break;
+        }
+
         const start = Date.now()
 
         const response = await request.get("https://restful-booker.herokuapp.com/ping")
@@ -24,9 +33,10 @@ test("Health monitor API" , async({request}) =>
             throw new Error("Api respinse is slow - " +Duration)
 
         }
-        else
+        else 
         {
             console.log("the total duration of the response is " +Duration)
+            
         }
 
         const responsestatus = await response.status()
@@ -34,6 +44,8 @@ test("Health monitor API" , async({request}) =>
         console.log("Response code of the api is " +responsestatus)
 
         expect(responsestatus).toBe(201)
+
+
 
     }
 
